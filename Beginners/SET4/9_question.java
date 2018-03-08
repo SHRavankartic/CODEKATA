5221a7741f957326aa8fb163dca0d7dca65144fd
Shravan Karthic A <7karthics@gmail.com>
2018-03-08 12:01:46 +0530

    Create 9_question.java

---
+++ Beginners/SET4/9_question.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+public class maxx{
+     public static void main(String aa[]){
+     int n;
+     int c;
+     Scanner s=new Scanner(System.in);
+     n=s.nextInt();
+     ArrayList<Integer>a=new ArrayList<Integer>();
+     for(int i=0;i<n;i++){
+        c=s.nextInt();
+        a.add(c);
+     }
+   System.out.print(Collections.max(a));
+   System.out.print("\n");
+    }
+    
+}
