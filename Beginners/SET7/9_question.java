d559ffc001b935b7935d6803e74b7610deb4da2e
Shravan Karthic A <7karthics@gmail.com>
2018-03-08 23:02:38 +0530

    Create 9_question.java

---
+++ Beginners/SET7/9_question.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class difo{
+     public static void main(String aa[]){
+     int a;
+     int b;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+     b=s.nextInt();
+     int c=a-b;
+     if(c%2==0){
+         System.out.println("Even\n");
+     }
+ else{
+           System.out.println("Odd\n");
+      }
+    }
+}
