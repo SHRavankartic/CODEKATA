58b0521b3701950f1f4f1304daf530e58550729e
Shravan Karthic A <7karthics@gmail.com>
2018-03-08 23:09:24 +0530

    Create 1_question.java

---
+++ Beginners/SET8/1_question.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class palin{
+     public static void main(String aa[]){
+         String a;
+    Scanner s=new Scanner(System.in);
+    a=s.next();
+    String w=new StringBuffer().reverse().toString();
+    if(a.equalsIgnoreCase(w)){
+        System.out.println("Palindrome\n");
+    }
+ else{
+        System.out.println("Not palindrome\n");
+      }
+  }
+}
