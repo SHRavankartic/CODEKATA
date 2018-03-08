f4ae462a13cbf128a04b2f8cb227c41ccca0db4a
Shravan Karthic A <7karthics@gmail.com>
2018-03-08 11:50:13 +0530

    Create 2_question.java

---
+++ Beginners/SET4/2_question.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+public class wordc{
+     public static void main(String aa[]){
+      String a;
+      int count=0;
+      Scanner s=new Scanner(System.in);
+      a=s.nextLine();
+     String d[]=a.split("\\s+");
+     for(int i=0;i<d.length;i++){
+         count++;
+     }
+     System.out.println(count);
+    }
+}
