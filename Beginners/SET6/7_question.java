b2f5d86d1703b65f40b0fb88208e4e8f881df0a2
Shravan Karthic A <7karthics@gmail.com>
2018-03-08 12:34:08 +0530

    Create 7_question.java

---
+++ Beginners/SET6/7_question.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+import java.lang.*;
+public class s {
+     public static void main(String aa[]){
+     int a;
+     int b;
+     int t;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+     b=s.nextInt();
+     t=a;
+     a=b;
+     b=t;
+     System.out.printf("%d %d\n",a,b);
+    }
+}
