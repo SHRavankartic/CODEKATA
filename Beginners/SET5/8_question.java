e688cadbeb1525ee24d493cc8ad5783bd8a5e312
Shravan Karthic A <7karthics@gmail.com>
2018-03-08 12:20:34 +0530

    Create 8_question.java

---
+++ Beginners/SET5/8_question.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class avg{
+     public static void main(String aa[]){
+     int a,f=0;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+   for(int i=1;i<=a;i++){
+       f=f+i;
+   }
+     int g=f/a;
+     System.out.println(g);
+     System.out.println("\n");
+    }
+}
