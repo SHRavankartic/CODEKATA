6c9feadb0cbbba4de6d04f20535b5cba9593a43e
Shravan Karthic A <7karthics@gmail.com>
2018-03-08 12:34:44 +0530

    Create 8_question.java

---
+++ Beginners/SET6/8_question.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+public class sbb{
+     public static void main(String aa[]){
+     int a;
+     int b;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+     b=s.nextInt();
+     a=a^b;
+     b=a^b;
+     a=a^b;
+     System.out.printf("%d %d\n",a,b);
+    }
+}
